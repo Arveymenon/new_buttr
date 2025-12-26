b10c0a70cc8f097ae43b530df5ba063c658489be
arveymenon <arveymenon@gmail.com>
2025-12-26 20:22:05 +0530

    initial push

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  /* 1 - below attributes added explicitly added here for github pages */
+  output: 'export',
+  images: {
+    unoptimized: true // REQUIRED for GitHub Pages
+  },
+  basePath: '/buttr.io',
+  assetPrefix: '/buttr.io'
+  /* end 1 */
 };
 
 export default nextConfig;
